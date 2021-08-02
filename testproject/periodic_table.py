8c7c9626e561d29bdf6d2537438784f0c9c132ba
Mazurka Zita <tmazurkazita@gmail.com>
2021-08-03 01:21:26 +0200

    add feladat

--- testproject/periodic_table.py
+++ testproject/periodic_table.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import time
+import csv
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
@@ -8,3 +9,23 @@ try:
     # oldal betöltése
     driver.get(" https://witty-hill-0acfceb03.azurestaticapps.net/periodic_table.html ")
     time.sleep(2)
+    # TC01 adatok beolvasás
+
+    with open('data.txt', encoding='utf-8') as csvtable:
+        csvreader = csv.reader(csvtable, delimiter=',')
+        for row in csvreader:
+            print(row)
+
+    """lis = []
+    period_elements = driver.find_elements_by_xpath("//div/ul/li")
+    for elem in period_elements:
+        lis.append(elem)
+        print()"""
+
+# nehéz feladat még nekem, több idő kellene rá. de valahogyan végigmennék az elemeken(data-pos) talán for ciklussal,
+# megnézném, hány elemből áll, majd assertel megnézném, hány elemből áll a data.txt fájl.
+#vagy összehasonlítanám, hogy megegyeznek-e a nevek.
+
+finally:
+    pass
+    # driver.close()
